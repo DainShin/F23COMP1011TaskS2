dc7d7caa763b7843a925519875d7609f0a25e752
DainShin <di.shin0611@gmail.com>
2023-10-09 10:38:04 -0400

    week 06 - Added view and controller for the TableView

---
+++ src/main/java/com/example/f23comp1011tasks2/TableViewController.java
@@ -0,0 +1,36 @@
+package com.example.f23comp1011tasks2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TableViewController {
+
+    @FXML
+    private TableColumn<?, ?> dueDateColumn;
+
+    @FXML
+    private TableColumn<?, ?> estimateColumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TableColumn<?, ?> taskIDColumn;
+
+    @FXML
+    private TableColumn<?, ?> titleColumn;
+
+    @FXML
+    private TableColumn<?, ?> userColumn;
+
+    @FXML
+    void viewCharts(ActionEvent event) {
+
+    }
+
+}
